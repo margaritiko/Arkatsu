9e87e04db661168c20979a0bf0055cff2eaaec69
Margarita <margaritiko@gmail.com>
2020-03-10 23:32:46 +0300

    Added additional urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import path
 
 from rest_framework import viewsets
+from rest_framework import routers
 from back.serializers import *
 from back.models import *
+from back import views
+from django.conf.urls import include, url
 
 # ViewSets define the view behavior.
 
@@ -34,6 +37,14 @@ class DailyBonusViewSet(viewsets.ModelViewSet):
     queryset = DailyBonus.objects.all()
     serializer_class = DailyBonusSerializer
 
+router = routers.DefaultRouter()
+router.register(r'students', Student)
+router.register(r'daily_bonus', DailyBonus)
+router.register(r'categories', Category)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('create_user/<username>', views.create_user),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
