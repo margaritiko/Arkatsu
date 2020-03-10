1ddab617993f5bfcd45eeab3c5bf70c28f07a929
Margarita <margaritiko@gmail.com>
2020-03-10 23:11:45 +0300

    New backend

---
+++ backend/backend/urls.py
@@ -0,0 +1,39 @@
+"""backend URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from rest_framework import viewsets
+from back.serializers import *
+from back.models import *
+
+# ViewSets define the view behavior.
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class DailyBonusViewSet(viewsets.ModelViewSet):
+    queryset = DailyBonus.objects.all()
+    serializer_class = DailyBonusSerializer
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
